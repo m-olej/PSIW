ed932eec4357397b2a853aa05d9c0acb76501727
m-olej <music.olejniczak>
2023-11-20 08:47:50 +0100

    reorganizing

---
+++ laby/lab4-Potoki/pipeDupCap.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/time.h>
+int main(){
+
+    int fd[2];
+    pipe( fd );
+
+    if (fork() == 0){
+        close(fd[0]);
+        dup2(fd[1],1);
+        execlp("ls", "ls", "-a", NULL);
+    }else
+    {
+        close(fd[1]);
+        dup2(fd[0],0);
+        execlp("tr", "tr", "a-z", "A-Z", NULL);
+    }
+        printf("%d", fd[0]);
+
+    return 0;
+}
